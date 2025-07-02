832d3e9d290705a38c0d3632ebfd85a5e81c7bb9
Zuveriya Khan <zuveriyap05@gmail.com>
2025-07-02 15:40:48 +0530

    Initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection"
-import { ContactSection } from "../components/COntactSection";
+import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
 
 export const Home = () => {
