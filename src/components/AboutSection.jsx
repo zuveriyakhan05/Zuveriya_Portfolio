f7e9190b83b590de2dd7c5a380a9854139485d29
Zuveriya <zuveriyap05@gmail.com>
2026-03-03 12:41:47 +0530

    Resume Update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -43,7 +43,7 @@ export const AboutSection = () => {
                                 Get In Touch
                             </a>
 
-                            <a href="/Zuveriya's Resume.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            <a href="/Pathan Zuveriya.pdf" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download CV
                             </a>
 
