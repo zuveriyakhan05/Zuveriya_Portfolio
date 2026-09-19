b138edb48e6ec561ec191d40f990c045846c0c7b
Zuveriya <zuveriyap05@gmail.com>
2026-09-19 23:37:59 +0530

    Fix allowed hosts for Render

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "dev-only-change-me"
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "testserver"]
+ALLOWED_HOSTS = [
+    "zuveriya-portfolio.onrender.com",
+    "localhost",
+    "127.0.0.1",
+    ]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
